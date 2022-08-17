b72798a43d19b495e77269be033e0ad33073b3a5
Branden <brandenconnected@gmail.com>
2022-08-17 13:10:09 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿using System.Windows.Forms;
+
+namespace Collatz_ChartVisualization
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dockManager1 = new DevExpress.XtraBars.Docking.DockManager(this.components);
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.dockManager2 = new DevExpress.XtraBars.Docking.DockManager(this.components);
+            this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dockManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dockManager2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(461, 136);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Collatz Conjecture Graph Calculator.\r\n\r\nMath Problem: \r\n3n+ 1\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(458, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(414, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter a number(Greater than 0)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(421, 138);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 72);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(638, 138);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(391, 72);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Activate Stock Ticker";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(193, 138);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(222, 72);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Reset Graph";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(661, 48);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 40);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Silver;
+            this.textBox2.Location = new System.Drawing.Point(1, 214);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(2336, 172);
+            this.textBox2.TabIndex = 7;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1, 138);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(186, 72);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Close App.";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // dockManager1
+            // 
+            this.dockManager1.Form = this;
+            this.dockManager1.TopZIndexControls.AddRange(new string[] {
+            "DevExpress.XtraBars.BarDockControl",
+            "DevExpress.XtraBars.StandaloneBarDockControl",
+            "System.Windows.Forms.MenuStrip",
+            "System.Windows.Forms.StatusStrip",
+            "System.Windows.Forms.StatusBar",
+            "DevExpress.XtraBars.Ribbon.RibbonStatusBar",
+            "DevExpress.XtraBars.Ribbon.RibbonControl",
+            "DevExpress.XtraBars.Navigation.OfficeNavigationBar",
+            "DevExpress.XtraBars.Navigation.TileNavPane",
+            "DevExpress.XtraBars.TabFormControl",
+            "DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl",
+            "DevExpress.XtraBars.ToolbarForm.ToolbarFormControl"});
+            // 
+            // chart2
+            // 
+            this.chart2.BackColor = System.Drawing.Color.Silver;
+            this.chart2.BorderlineWidth = 0;
+            chartArea1.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisX.IsLabelAutoFit = false;
+            chartArea1.AxisX.MaximumAutoSize = 100F;
+            chartArea1.AxisX2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea1.AxisX2.MaximumAutoSize = 50F;
+            chartArea1.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisY.IsLabelAutoFit = false;
+            chartArea1.AxisY.MaximumAutoSize = 50F;
+            chartArea1.AxisY2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea1.AxisY2.MaximumAutoSize = 50F;
+            chartArea1.CursorX.LineColor = System.Drawing.Color.Blue;
+            chartArea1.CursorY.LineColor = System.Drawing.Color.Blue;
+            chartArea1.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea1);
+            this.chart2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.chart2.Location = new System.Drawing.Point(0, 0);
+            this.chart2.Margin = new System.Windows.Forms.Padding(0);
+            this.chart2.Name = "chart2";
+            this.chart2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.IsVisibleInLegend = false;
+            series1.IsXValueIndexed = true;
+            series1.LabelBorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            series1.LabelForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            series1.Name = "Series1";
+            series1.SmartLabelStyle.Enabled = false;
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            this.chart2.Series.Add(series1);
+            this.chart2.Size = new System.Drawing.Size(2997, 1111);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart1";
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.label1);
+            this.panelControl1.Controls.Add(this.label2);
+            this.panelControl1.Controls.Add(this.button1);
+            this.panelControl1.Controls.Add(this.button2);
+            this.panelControl1.Controls.Add(this.button5);
+            this.panelControl1.Controls.Add(this.button4);
+            this.panelControl1.Controls.Add(this.textBox2);
+            this.panelControl1.Controls.Add(this.textBox1);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelControl1.Location = new System.Drawing.Point(0, 1114);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(2997, 388);
+            this.panelControl1.TabIndex = 15;
+            // 
+            // dockManager2
+            // 
+            this.dockManager2.Form = this;
+            this.dockManager2.TopZIndexControls.AddRange(new string[] {
+            "DevExpress.XtraBars.BarDockControl",
+            "DevExpress.XtraBars.StandaloneBarDockControl",
+            "System.Windows.Forms.MenuStrip",
+            "System.Windows.Forms.StatusStrip",
+            "System.Windows.Forms.StatusBar",
+            "DevExpress.XtraBars.Ribbon.RibbonStatusBar",
+            "DevExpress.XtraBars.Ribbon.RibbonControl",
+            "DevExpress.XtraBars.Navigation.OfficeNavigationBar",
+            "DevExpress.XtraBars.Navigation.TileNavPane",
+            "DevExpress.XtraBars.TabFormControl",
+            "DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl",
+            "DevExpress.XtraBars.ToolbarForm.ToolbarFormControl"});
+            // 
+            // form1BindingSource
+            // 
+            this.form1BindingSource.DataSource = typeof(Collatz_ChartVisualization.Form1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2997, 1502);
+            this.Controls.Add(this.panelControl1);
+            this.Controls.Add(this.chart2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dockManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            this.panelControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dockManager2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer1;
+        private DevExpress.XtraBars.Docking.DockManager dockManager1;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private BindingSource form1BindingSource;
+        private DevExpress.XtraBars.Docking.DockManager dockManager2;
+        public System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿namespace Collatz_ChartVisualization
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Windows.Forms;
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private Random StockRandom = new Random();
+
+        private double StockPriceRandom = new double();
+
+        private List<double> StockPriceList = new List<double>();
+
+
+        //------------ BUTTONS --------------//
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            long max = Convert.ToInt64(textBox1.Text);
+            if (max > 0)
+                CalculateSequence(max);
+            else
+                MessageBox.Show("Insert a number greater than zero!");
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {   
+           string sequence = RandomStock().ToString() + ";";
+
+            for (int i = 1; i < 200; i++)
+            {
+                chart2.Series[0].Points.AddY(RandomStock());
+                sequence += RandomStock().ToString() + "; ";
+
+            }
+            textBox2.Text = sequence;
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            InitializeGraph();
+            textBox1.Clear();
+            MessageBox.Show("Graph Successfully Reset !");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Goodbye");
+            Application.Exit();
+        }
+
+        //------------ FUNCTIONS --------------//
+
+        private void CalculateSequence(long max)
+        {
+            InitializeGraph();
+            string sequence = max.ToString() + "; ";
+            chart2.Series[0].Points.AddY(max);
+            do
+            {
+                max = (max % 2 == 0) ? max / 2 : max * 3 + 1;
+                sequence += max.ToString() + "; ";
+                chart2.Series[0].Points.AddY(max);
+            } while (max != 1);
+            textBox2.Text = sequence;
+        }
+
+        private void InitializeGraph()
+        {
+            chart2.Series[0].Points.Clear();
+            textBox2.Text = "";
+        }
+
+        private double RandomStock()
+        {  
+            double random = StockRandom.Next(-2, 3);
+            StockPriceRandom = Math.Round(StockPriceRandom + random);
+            StockPriceList.Add(StockPriceRandom);
+            return StockPriceRandom;          
+
+        }
+
+    }
+}
